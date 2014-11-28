e32fea03434059418a453002ced2d335d0c8274d
unknown <klasen@gmx.net>
2014-11-28 01:14:49 +0100

    use curlHandle to keep session add createSearchJob function

--- R/RSentinel.R
+++ R/RSentinel.R
@@ -2,26 +2,53 @@ library(RCurl)
 library(rjson)
 
 
-options(RCurlOptions = list(userpwd = paste(username, password, sep=":"),
-                            httpauth = 1L,
-                            timeout = 200,
-                            connecttimeout = 10,
-                            verbose = TRUE,
-                            useragent = "RSentinel",
-                            sslversion = SSLVERSION_TLSv1,
-                            ssl.verifypeer = FALSE,
-                            ssl.verifyhost=FALSE))
+opts <- curlOptions(
+  timeout = 120,
+  connecttimeout = 5,
+  verbose = TRUE,
+  useragent = "RSentinel/0.1",
+  sslversion = SSLVERSION_TLSv1,
+  ssl.verifypeer = FALSE,
+  ssl.verifyhost=FALSE,
+  customrequest = "GET"
+)
 
-authJSON<-httpPOST( paste("https://", server, ":8443/SentinelAuthServices/auth/tokens", sep="") )
+ch <- getCurlHandle()
+
+authJSON<-getURL( 
+  paste("https://", server, ":8443/SentinelAuthServices/auth/tokens", sep=""),
+  customrequest = "POST",
+  userpwd = paste(username, password, sep=":"),
+  httpauth = AUTH_BASIC,
+  .opts = opts, 
+  curl = ch 
+)
 
 samlToken <- fromJSON(authJSON)$Token
 authorization <- paste("X-SAML",samlToken, sep=" ")
 
-collectorJSON <-  httpGET(
-  paste("https://", server, ":8443/SentinelRESTServices/objects/collector", sep=""),
+opts <- curlOptions(
   httpheader = c(
-    "Authorization"=authorization
+    "Authorization"=authorization,
+    "Accept"="application/json"#,
+    #"Content-Type"="application/json"
   )
+)
+
+opts.post <- curlOptions(
+  httpheader = c(
+    opts$httpheader,
+    "Content-Type"="application/json"
+  )
+)
+
+
+collectorJSON <-  getURL(
+  paste("https://", server, ":8443/SentinelRESTServices/objects/collector", sep=""),
+  customrequest = "GET",
+  #httpheader = c( "Authorization"=authorization)
+  .opts = opts,
+  curl = ch
 )
 
 getCollectorDataframeFromJSON <- function(collectorJSON) {
@@ -43,9 +70,56 @@ getCollectorDataframeFromJSON <- function(collectorJSON) {
 
 cdf <- getCollectorDataframeFromJSON(collectorJSON)
 
-epsJSON <-  httpGET(
+epsJSON <- getURL(
   paste("https://", server, ":8443/SentinelRESTServices/objects/eps-history", sep=""),
-  httpheader = c(
-    "Authorization"=authorization
-  ), verbose = F
+  customrequest = "GET",
+  .opts = opts,
+  curl = ch
 )
+
+createSearchJob <- function(filter) {
+  params <- list(
+    "filter"=filter,
+    "start"="2014-11-27T20:08:34.940Z",
+    "end"  ="2014-11-27T21:08:34.940Z",
+    "pgsize"=125,
+    "max-results"=50000,
+    "type"="user"
+  )
+  content <- getURL(
+    paste("https://", server, ":8443/SentinelRESTServices/objects/event-search", sep=""),
+    customrequest = "POST",
+    postfields = toJSON(params),
+    #httpheader = c( "Content-Type"="application/json", opts$httpheader),
+    .opts = opts.post,
+    curl = ch
+  )
+  job <- NULL
+  if (getCurlInfo(ch)$response.code == 201) {
+    job <- fromJSON(content)
+  }
+  return(job)
+}
+
+createSearchTermJob <- function(job) {
+  params <- list(
+    "meta"=list("type"="search-terms"),
+    "event-search"=list("@href"=job$meta$"@href"),
+    "field-names"= list("evt", "sip")
+  )
+  content <- getURL(
+    paste("https://", server, ":8443/SentinelRESTServices/objects/search-terms", sep=""),
+    customrequest = "POST",
+    postfields = toJSON(params),
+    #httpheader = c( "Content-Type"="application/json", opts$httpheader),
+    .opts = opts.post,
+    curl = ch
+  )
+  job <- NULL
+  if (getCurlInfo(ch)$response.code == 201) {
+    job <- fromJSON(content)
+  }
+  return(job)
+}
+
+
